2a948a9873029b1ff6ab2bd59c0eb6cecf277bb9
Ece Kahraman <ece.kahraman@ug.bilkent.edu.tr>
2023-12-18 23:08:47 +0300

    annen

--- assignment_3/script.js
+++ assignment_3/script.js
@@ -1,91 +1,119 @@
 var canvas;
 var gl;
 var program;
-var positions = [];
+var strips = [];
+var oneStrip = [];
 var colors = [];
-var projectionMatrixLoc;
-var modelViewMatrixLoc;
+var vertexColors = [
+    [ 0.0, 0.0, 0.0, 1.0 ],  // black
+    [ 1.0, 0.0, 0.0, 1.0 ],  // red
+    [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+    [ 0.0, 1.0, 0.0, 1.0 ],  // green
+    [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+    [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+    [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+    [ 1.0, 1.0, 1.0, 1.0 ]   // white
+];
 var vBuffer;
 
-function x(u, v, aa) {
+var left = -2.0;
+var right = 2.0;
+var ytop = 2.0;
+var bottom = -2.0;
+var radius = 1.0;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var eye;
+
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+var numUPoints = 50, numVPoints = 50;
+
+const aa = 0.8;
+
+function x(u, w) {
+    var denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
+    return -1*u + (2*(1 - Math.pow(aa, 2))*Math.cosh(aa*u)*Math.sin(aa*u)) / denom;
     
-    let w = Math.sqrt(1 - Math.pow(aa, 2));
-    let denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
-    return (-u + (2*(1 - Math.pow(aa, 2))*Math.cosh(aa*u)*Math.sin(aa*u)) / denom);
 }
 
-function y(u, v, aa) {
-    let w = Math.sqrt(1 - Math.pow(aa, 2));
-    let denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
-    return (2*w*Math.cosh(aa*u)*(-w*Math.cos(v)*Math.cos(w*v)-Math.sin(v)*Math.sin(w*v)))/denom;
+function y(u, v, w) {
+    var denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
+    return (2*w*Math.cosh(aa*u)*(-1*w*Math.cos(v)*Math.cos(w*v)-Math.sin(v)*Math.sin(w*v)))/denom;
 }
 
-function z(u, v, aa) {
-    let w = Math.sqrt(1 - Math.pow(aa, 2));
-    let denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
-    return (2*w*Math.cosh(aa*u)*(-w*Math.sin(v)*Math.cos(w*v)+Math.cos(v)*Math.sin(w*v)))/denom;
+function z(u, v, w) {
+    var denom = aa * (Math.pow(w*Math.cosh(aa*u), 2) + Math.pow(aa*Math.sin(w*u), 2));
+    return (2*w*Math.cosh(aa*u)*(-1*w*Math.sin(v)*Math.cos(w*v)+Math.cos(v)*Math.sin(w*v)))/denom;
 }
 
 window.onload = function init() {
     canvas = document.getElementById('canvas');
     gl = canvas.getContext('webgl');
 
+    gl.viewport( 0, 0, canvas.width, canvas.height );    
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.enable(gl.DEPTH_TEST);
+
+    
+    
+    const minUV = -10, maxUV = 10;
+    const step = (maxUV - minUV)/250;
+    let w = Math.sqrt(1 - Math.pow(aa, 2));
+    
+    for (let u = minUV; u < maxUV; u += step) {
+        for (let v = minUV; v < maxUV; v += step) {            
+            oneStrip.push(...[x(u, w)/8, y(u, v, w)/8, z(u, v, w)/8,
+            x(u+step, w)/8, y(u+step, v, w)/8, z(u+step, v, w)/8]);
+            colors.push(vertexColors[Math.floor(Math.random()*8)]);
+            colors.push(vertexColors[Math.floor(Math.random()*8)]);
+            
+        }
+        strips.push(...oneStrip);
+        oneStrip = [];
+    }
+
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-
-
-    for (var u = -14; u <= 14; u += 0.1) {
-        for (var v = -37.4; v < 37.4; v += 0.1) {
-            //let x1 = (x(u, v, 0.5)/Math.PI) + 1
-            //let y1 = (y(u, v, 0.5)/Math.PI) + 1
-            //let z1 = (z(u, v, 0.5)/Math.PI) + 1
-            positions.push(vec4(x(u, v, 0.4), y(u, v, 0.4), z(u, v, 0.4), 1.0));
-            //console.log(x1, y1, z1)
-            //positions.push(y(u, v, 0.5));
-            //positions.push(z(u, v, 0.5));
-            //positions.push(1);
-        }
-    }
-
-
-
     vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(strips), gl.STATIC_DRAW);
     
-    var vPosition = gl.getAttribLocation( program, "aVertexPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    var vPosition = gl.getAttribLocation( program, "position" );
     gl.enableVertexAttribArray( vPosition );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+    
 
 
-    var buffers = {
-        position: vBuffer,
-        numVertices: positions.length/3,
-    };
-    drawScene(gl, buffers);
+    render();
 }
 
-function drawScene(gl, buffers) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clearDepth(1.0);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
+var render = function(){
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            
+    //eye = vec3(radius*Math.sin(theta)*Math.cos(phi), radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    //modelViewMatrix = lookAt(eye, at , up);
+    projectionMatrix = mat4(); //ortho(left, right, bottom, ytop, near, far);
+            
+    //gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    const fieldOfView = 45 * Math.PI / 180;
-    const aspect = canvas.width / canvas.height;
-    const zNear = 0;
-    const zFar = 100;
-    var projection = mat4();
-    projection = perspective(fieldOfView, aspect, zNear, zFar);
-
-    var modelView = mat4();
-    modelView = translate( 0.0, 0.0, -600.0);
-
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projection) );
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelView) );
-
-    gl.drawArrays(gl.LINE_STRIP, 0, buffers.numVertices);
+// render columns of data then rows
+        
+    for(let i=0; i<strips.length; i++) gl.drawArrays( gl.TRIANGLE_STRIP, i*606, 606 );            
+    //for(var i=0; i<numVPoints; i++) gl.drawArrays( gl.LINE_STRIP, i*numUPoints+positions.length/2, numUPoints );
+    //gl.drawArrays(gl.TRIANGLES_STRIP, 0, positions.length / 3);
+    //requestAnimFrame(render);
 }
